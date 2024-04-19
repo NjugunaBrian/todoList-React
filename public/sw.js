de742769d52577fec00d3b38d1177d7af8d9b9e9
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-04-19 15:46:25 +0300

    feat: cache storage for offline asset caching

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,25 @@
-console.warn("ws file is public folder")
+let todosCache = 'my-cache';
+
+this.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(todosCache).then((cache) => {
+            cache.addAll([
+                '/static/js/bundle.js',
+                '/taskLogo.jpg',
+                '/ws',
+                '/index.html',
+                '/'
+            ])
+        })
+    )
+})
+
+this.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            if (response){
+                return response
+            }
+        })
+    )
+})
