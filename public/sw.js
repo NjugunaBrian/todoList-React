c61f0a8025da57c32d3c44870793e46ed89cbef1
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-04-19 15:10:08 +0300

    fix: fixed import error

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+console.warn("ws file is public folder")
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import register from './serviceWorkerRegistration';
+import swDev from './swDev';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App />);
 
-register.register();
+swDev();
 
 
